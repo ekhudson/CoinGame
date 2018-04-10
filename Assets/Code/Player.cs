5b712187265c58508fb2b3a8107e468504387fd0
Elliot Hudson <ekhudson@gmail.com>
2018-04-09 23:04:03 -0700

    Starting work on player profile

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,19 @@
+﻿
+
+[System.Serializable]
+public class Player
+{
+    private string mName;
+
+    public string Name
+    {
+        get
+        {
+            return mName;
+        }
+        set
+        {
+            mName = value;
+        }
+    }
+}
---
+++ Assets/Code/SessionManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PlayerSessionData
+{
+    public Player Player;
+    public List<CoinScript> CoinsCollected;
+}
+
+public class SessionManager : MonoBehaviour
+{
+    private List<Player> mPlayerList;
+
+    private int mCurrentPlayerIndex = 0;
+
+    public List<Player> PlayerList
+    {
+        get
+        {
+            return mPlayerList;
+        }
+    }
+}
